bc3f1f55c0acfe7358e76be90603d10585a8eb71
Moody Rahman <moody.byte@gmail.com>
2021-03-06 17:17:33 -0500

    generate new uid only once

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, TextField } from "@material-ui/core";
+import { useState } from "react";
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 
 function App() {
+  const [uid, setuid] = useState("");
+  if (localStorage.getItem("uid") === null || localStorage.getItem("uid") === "" ) {
+    setuid(makeid(10));
+    localStorage.setItem("uid", uid);
+  }
+  localStorage.setItem("piss", "poss")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <form noValidate autoComplete="off">
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+      </form>
+
+      <button onClick={(e)=>{
+        localStorage.removeItem("uid");
+      }}>
+        click to reset the storage
+      </button>
+      {localStorage.getItem("uid")}
+    </Container>
   );
 }
 
