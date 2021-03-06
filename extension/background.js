f80009c1596a7db76b78ba8ce14651b1f7f14ef4
Moody Rahman <moody.byte@gmail.com>
2021-03-06 13:10:20 -0500

    chrome popup console logs

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,9 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
+alert("sdfsdf")
+
+document.addEventListener("DOMContentLoaded", () => {
+    var button = document.getElementById("changeColor")
+   
+    button.addEventListener("click", (e) => {
+      console.log(e)
+    })
+})
