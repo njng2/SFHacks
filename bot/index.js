1aaf74acedd735d5d15d31610794068ea90009c0
Moody Rahman <moody.byte@gmail.com>
2021-03-05 19:30:52 -0500

    nuke

--- bot/index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-require('dotenv').config()
-
-const client = new Discord.Client();
-
-console.log(process.env.BOT_TOKEN)
-
-client.login(process.env.BOT_TOKEN);
