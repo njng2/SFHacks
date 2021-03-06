fc72278bbdfbc8110a8ab1495392704a7fce34df
Moody Rahman <moody.byte@gmail.com>
2021-03-06 13:40:04 -0500

    add message listeners

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,8 @@
+console.log("hello")
 alert("sdfsdf")
 
-document.addEventListener("DOMContentLoaded", () => {
-    var button = document.getElementById("changeColor")
-   
-    button.addEventListener("click", (e) => {
-      console.log(e)
-    })
-})
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document)
+});
--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,9 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+     console.log("HEWRM")
+   }
+);
+
 document.getElementById("changeColor").onclick = (event) => {
     console.log("death")
 }
